6ccb93bd16e1c39ddc13c1a8535dad75979434f4
vishaalprasad <vishaal.prasad@gmail.com>
2016-09-11 16:10:09 -0700

    Metropolis Hastings Algorithm for MCMCs
    Toy example that is hard-coded to work for Gaussians. The function to be sampled from is a standard Gaussian by default: mean is 0 and standard deviation is 1. The proposal distribution is also a Gaussian.
    
    Keeping the sampling function's parameters constant and changing the proposal distribution's parameters is an easy way to gain key intuitions about how the Metropolis-Hastings algorithm works. This can be easily generalized to functions in general, but for didactic purposes, this should suffice.

---
+++ metropolis_hastings.py
@@ -0,0 +1,88 @@
+'''
+Metropolis-Hastings: an MCMC method when you can't sample directly from the
+dataset. Why not rejection sampling or something similar? When the distribution
+is high-dimensional, the acceptance ratio falls low. Or we can't (for whatever
+reason) can't sample IID. We need another method that isn't beholden to IID. 
+
+This can be done via Markov Chains, where we choose correlated data so that
+we stay in high-probability areas. Enter Metropolis-Hastings.
+
+Acknowledgement to http://mlwhiz.com/blog/2015/08/19/MCMC_Algorithms_Beta_Distribution/
+which helped especially with the visualization. 
+'''
+import numpy as np
+import random
+import math
+import matplotlib.pyplot as plt 
+import matplotlib.mlab as mlab 
+import scipy.stats 
+
+#First, define useful helper methods.
+
+#Gaussian method: this will be used for what we are sampling
+def gauss_pdf(x, mean=0,stddev=1.0):
+	return math.exp(-(x-mean)**2/(2*stddev**2)) #don't worry about norm const!
+
+#Given some probabliity p, determine whether to accept or reject.
+def standard_uniform(p):
+	if p >= random.uniform(0,1):
+		return True
+	else:
+		return False
+
+'''
+Actual Metropolis Hastings algorithm.
+Hard-coded so the function we are sampling from is standard Gaussian.
+Hard-coded so the proposal distribution is zero-centered Gaussian with variable sigma. 
+
+Param List:
+Initial: The initial guess. Arbitrarily let 0 be default.
+Iterations: The total number of iterations M-H runs for. 
+Count: The count of actually used iterations; the last "count" iterations.
+Note that Count < Iterations necessarily.
+True mean, sigma: The parameters for our true model that we are sampling from.
+
+
+'''
+def gaussian_metropolis_hastings(
+								initial=0, 
+								iterations=10000, 
+								count=5000, 
+								proposal_mean=0,
+								proposal_sigma=1.0,
+								true_mean=0,
+								true_sigma=1.0):
+
+	states = []
+	states.append(initial)
+	curr = initial
+	for i in range (0, iterations):
+		proposal = scipy.stats.norm.rvs(proposal_mean, proposal_sigma)
+		ap = min(1, gauss_pdf(proposal, true_mean, true_sigma)/
+			gauss_pdf(curr, true_mean, true_sigma))
+		if standard_uniform(ap):
+			curr = proposal
+		states.append(curr)
+
+	return states[-count:]
+
+# Visualize the gaussian metropolis hastings
+def visualize_gaussian_metropolis_hastings_estimate(mean, sigma):
+	ys = []
+	xs = []
+	i_list = np.mgrid[-5:5:200j]
+	for i in i_list:
+		xs.append(i)
+		ys.append(scipy.stats.norm(0, 1).pdf(i))
+	plt.plot(xs, ys, label='True Gaussian Distribution: mean='+str(mean)+
+		', sigma='+str(sigma))
+	res = gaussian_metropolis_hastings(iterations = 1000000, count = 800000, proposal_sigma = 5, true_mean = mean, true_sigma = sigma)
+	plt.hist(res, normed=True, bins=100, histtype='step', label='Metropolis-Hastings Approximation')
+	plt.legend()
+	plt.show()
+
+
+if __name__ == '__main__':
+	visualize_gaussian_metropolis_hastings_estimate(0, 1.0)
+
+
